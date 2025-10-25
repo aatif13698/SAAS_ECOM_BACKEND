f114ba850186736dcb470411a8e8751aa2626d73
aatif13698 <aatif13698@gmail.com>
2025-10-25 10:15:31 +0530

    leave category correction

--- VendorHr/routes/leaveCategories.route.js
+++ VendorHr/routes/leaveCategories.route.js
@@ -1,27 +1,23 @@
+const express = require("express"); 
+let router = express.Router(); 
 
 
-const express = require("express");
-let router = express.Router();
+const leaveCategoryController = require("../controller/leaveCategories.controller"); 
+const entityAuth = require("../../middleware/authorization/commonEntityAuthorization/commonEntityAuthorization"); 
+const { uploadBranchIcon } = require("../../utils/multer"); 
 
 
+// # create, update, view, list, activate/inactive leave category 
 
-const leaveCategoryController = require("../controller/leaveCategories.controller");
-const entityAuth = require("../../middleware/authorization/commonEntityAuthorization/commonEntityAuthorization");
-const { uploadBranchIcon } = require("../../utils/multer");
+router.post('/create/leave/category', entityAuth.authorizeEntity("Administration", "Employee", "create"), leaveCategoryController.create); 
 
+router.put('/update/leave/category', entityAuth.authorizeEntity("Administration", "Employee", "update"), leaveCategoryController.update); 
 
-// # create, update, view, list, activate/inactive leave category
+router.get('/get/:clientId/:leave/category', entityAuth.authorizeEntity("Administration", "Employee", "create"), leaveCategoryController.getParticular); 
 
-router.post('/create/leave/category', entityAuth.authorizeEntity("Administration", "Employee", "create"), leaveCategoryController.create);
+router.get('/list/leave/category', entityAuth.authorizeEntity("Administration", "Employee", "create"), leaveCategoryController.list); 
 
-router.put('/update/leave/category', entityAuth.authorizeEntity("Administration", "Employee", "update"), leaveCategoryController.update);
-
-router.get('/get/:clientId/:leave/category', entityAuth.authorizeEntity("Administration", "Employee", "create"), leaveCategoryController.getParticular);
-
-router.get('/list/leave/category', entityAuth.authorizeEntity("Administration", "Employee", "create"), leaveCategoryController.list);
-
-router.post("/activeInactive/leave/category", entityAuth.authorizeEntity("Administration", "Employee", "create"), leaveCategoryController.activeinactive);
+router.post("/activeInactive/leave/category", entityAuth.authorizeEntity("Administration", "Employee", "create"), leaveCategoryController.activeinactive); 
 
 
-
-exports.router = router;
+exports.router = router; 
--- VendorHr/service/leaveCategories.service.js
+++ VendorHr/service/leaveCategories.service.js
@@ -1,99 +1,91 @@
-// services/chairService.js
-const { getClientDatabaseConnection } = require("../../db/connection");
-const message = require("../../utils/message");
-const statusCode = require("../../utils/http-status-code");
-const CustomError = require("../../utils/customeError");
-const clientAssetSchema = require("../../client/model/asset");
-const leaveCategorySchema = require("../../client/model/leaveCategories");
+// services/chairService.js 
+const { getClientDatabaseConnection } = require("../../db/connection"); 
+const message = require("../../utils/message"); 
+const statusCode = require("../../utils/http-status-code"); 
+const CustomError = require("../../utils/customeError"); 
+const clientAssetSchema = require("../../client/model/asset"); 
+const leaveCategorySchema = require("../../client/model/leaveCategories"); 
 
 
-const create = async (clientId, data) => {
-    try {
-        const clientConnection = await getClientDatabaseConnection(clientId);
-        const LeaveCategory = clientConnection.model('leaveCategory', leaveCategorySchema);
-        return await LeaveCategory.create(data);
-    } catch (error) {
-        throw new CustomError(error.statusCode || 500, `Error creating leave category : ${error.message}`);
-    }
-};
+const create = async (clientId, data) => { 
+    try { 
+        const clientConnection = await getClientDatabaseConnection(clientId); 
+        const LeaveCategory = clientConnection.model('leaveCategory', leaveCategorySchema); 
+        return await LeaveCategory.create(data); 
+    } catch (error) { 
+        throw new CustomError(error.statusCode || 500, `Error creating leave category : ${error.message}`); 
+    } 
+}; 
 
-const update = async (clientId, leaveCategoryId, updateData) => {
-    try {
-        const clientConnection = await getClientDatabaseConnection(clientId);
-        const LeaveCategory = clientConnection.model('leaveCategory', leaveCategorySchema);
-        const leaveCategory = await LeaveCategory.findById(leaveCategoryId);
-        if (!leaveCategory) {
-            throw new CustomError(statusCode.NotFound, message.lblLeaveCategoryNotFound);
-        }
-        Object.assign(leaveCategory, updateData);
-        await leaveCategory.save();
-        return leaveCategory
-    } catch (error) {
-        throw new CustomError(error.statusCode || 500, `Error updating leave category: ${error.message}`);
-    }
-};
-
-
-
-const getById = async (clientId, leaveCategoryId) => {
-    try {
-        const clientConnection = await getClientDatabaseConnection(clientId);
-        const LeaveCategory = clientConnection.model('leaveCategory', leaveCategorySchema);
-        const leaveCategory = await LeaveCategory.findById(leaveCategoryId);
-        if (!leaveCategory) {
-            throw new CustomError(statusCode.NotFound, message.lblLeaveCategoryNotFound);
-        }
-        return leaveCategory;
-    } catch (error) {
-        throw new CustomError(error.statusCode || 500, `Error getting leave category: ${error.message}`);
-    }
-};
+const update = async (clientId, leaveCategoryId, updateData) => { 
+    try { 
+        const clientConnection = await getClientDatabaseConnection(clientId); 
+        const LeaveCategory = clientConnection.model('leaveCategory', leaveCategorySchema); 
+        const leaveCategory = await LeaveCategory.findById(leaveCategoryId); 
+        if (!leaveCategory) { 
+            throw new CustomError(statusCode.NotFound, message.lblLeaveCategoryNotFound); 
+        } 
+        Object.assign(leaveCategory, updateData); 
+        await leaveCategory.save(); 
+        return leaveCategory 
+    } catch (error) { 
+        throw new CustomError(error.statusCode || 500, `Error updating leave category: ${error.message}`); 
+    } 
+}; 
 
 
-const list = async (clientId, filters = {}, options = { page: 1, limit: 10 }) => {
-    try {
-        const clientConnection = await getClientDatabaseConnection(clientId);
-        const LeaveCategory = clientConnection.model('leaveCategory', leaveCategorySchema);
-        const { page, limit } = options;
-        const skip = (page - 1) * limit;
-        const [leaveCategories, total] = await Promise.all([
-            LeaveCategory.find(filters).skip(skip),
-            LeaveCategory.countDocuments(filters),
-        ]);
-        return { count: total, leaveCategories };
-    } catch (error) {
-        throw new CustomError(error.statusCode || 500, `Error listing leave category: ${error.message}`);
-    }
-};
+const getById = async (clientId, leaveCategoryId) => { 
+    try { 
+        const clientConnection = await getClientDatabaseConnection(clientId); 
+        const LeaveCategory = clientConnection.model('leaveCategory', leaveCategorySchema); 
+        const leaveCategory = await LeaveCategory.findById(leaveCategoryId); 
+        if (!leaveCategory) { 
+            throw new CustomError(statusCode.NotFound, message.lblLeaveCategoryNotFound); 
+        } 
+        return leaveCategory; 
+    } catch (error) { 
+        throw new CustomError(error.statusCode || 500, `Error getting leave category: ${error.message}`); 
+    } 
+}; 
 
 
-const activeInactive = async (clientId, leaveCategoryId, data) => {
-    try {
-        const clientConnection = await getClientDatabaseConnection(clientId);
-        const LeaveCategory = clientConnection.model('leaveCategory', leaveCategorySchema);
-        const leaveCategory = await LeaveCategory.findById(leaveCategoryId);
-        if (!leaveCategory) {
-            throw new CustomError(statusCode.NotFound, message.lblLeaveCategoryNotFound);
-        }
-        Object.assign(leaveCategory, data);
-        return await leaveCategory.save();
-    } catch (error) {
-        throw new CustomError(error.statusCode || 500, `Error active inactive: ${error.message}`);
-    }
-};
+const list = async (clientId, filters = {}, options = { page: 1, limit: 10 }) => { 
+    try { 
+        const clientConnection = await getClientDatabaseConnection(clientId); 
+        const LeaveCategory = clientConnection.model('leaveCategory', leaveCategorySchema); 
+        const { page, limit } = options; 
+        const skip = (page - 1) * limit; 
+        const [leaveCategories, total] = await Promise.all([ 
+            LeaveCategory.find(filters).skip(skip), 
+            LeaveCategory.countDocuments(filters), 
+        ]); 
+        return { count: total, leaveCategories }; 
+    } catch (error) { 
+        throw new CustomError(error.statusCode || 500, `Error listing leave category: ${error.message}`); 
+    } 
+}; 
 
 
+const activeInactive = async (clientId, leaveCategoryId, data) => { 
+    try { 
+        const clientConnection = await getClientDatabaseConnection(clientId); 
+        const LeaveCategory = clientConnection.model('leaveCategory', leaveCategorySchema); 
+        const leaveCategory = await LeaveCategory.findById(leaveCategoryId); 
+        if (!leaveCategory) { 
+            throw new CustomError(statusCode.NotFound, message.lblLeaveCategoryNotFound); 
+        } 
+        Object.assign(leaveCategory, data); 
+        return await leaveCategory.save(); 
+    } catch (error) { 
+        throw new CustomError(error.statusCode || 500, `Error active inactive: ${error.message}`); 
+    } 
+}; 
 
 
-
-
-
-
-
-module.exports = {
-    create,
-    update,
-    getById,
-    list,
-    activeInactive,
-};
+module.exports = { 
+    create, 
+    update, 
+    getById, 
+    list, 
+    activeInactive, 
+}; 
