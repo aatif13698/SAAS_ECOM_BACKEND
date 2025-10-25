957aa2b59342b5d52ba6d56f07d982b5506b01fc
aatif13698 <aatif13698@gmail.com>
2025-10-25 10:09:02 +0530

    holiday correction

--- VendorHr/routes/holiday.route.js
+++ VendorHr/routes/holiday.route.js
@@ -1,27 +1,23 @@
+const express = require("express"); 
+let router = express.Router(); 
 
 
-const express = require("express");
-let router = express.Router();
+const holidayController = require("../controller/holiday.controller"); 
+const entityAuth = require("../../middleware/authorization/commonEntityAuthorization/commonEntityAuthorization"); 
+const { uploadBranchIcon } = require("../../utils/multer"); 
 
 
+// # create, update, view, list, activate/inactive holiday 
 
-const holidayController = require("../controller/holiday.controller");
-const entityAuth = require("../../middleware/authorization/commonEntityAuthorization/commonEntityAuthorization");
-const { uploadBranchIcon } = require("../../utils/multer");
+router.post('/create/holiday', entityAuth.authorizeEntity("Administration", "Employee", "create"), holidayController.create); 
 
+router.put('/update/holiday', entityAuth.authorizeEntity("Administration", "Employee", "update"), holidayController.update); 
 
-// # create, update, view, list, activate/inactive holiday
+router.get('/get/:clientId/:holiday', entityAuth.authorizeEntity("Administration", "Employee", "create"), holidayController.getParticular); 
 
-router.post('/create/holiday', entityAuth.authorizeEntity("Administration", "Employee", "create"), holidayController.create);
+router.get('/list/holiday', entityAuth.authorizeEntity("Administration", "Employee", "create"), holidayController.list); 
 
-router.put('/update/holiday', entityAuth.authorizeEntity("Administration", "Employee", "update"), holidayController.update);
-
-router.get('/get/:clientId/:holiday', entityAuth.authorizeEntity("Administration", "Employee", "create"), holidayController.getParticular);
-
-router.get('/list/holiday', entityAuth.authorizeEntity("Administration", "Employee", "create"), holidayController.list);
-
-router.post("/activeInactive/holiday", entityAuth.authorizeEntity("Administration", "Employee", "create"), holidayController.activeinactive);
+router.post("/activeInactive/holiday", entityAuth.authorizeEntity("Administration", "Employee", "create"), holidayController.activeinactive); 
 
 
-
-exports.router = router;
+exports.router = router; 
