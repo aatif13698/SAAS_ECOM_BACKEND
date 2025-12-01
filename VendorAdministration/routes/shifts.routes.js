9a67d8a6c1c363d07358c1ae5495a4b921561195
Md Shahbaz Ahmad <mdshahbazahmad@Mds-Mac-mini.local>
2025-12-01 17:42:40 +0530

    shift api correction

--- VendorAdministration/routes/shifts.routes.js
+++ VendorAdministration/routes/shifts.routes.js
@@ -7,15 +7,15 @@ const { uploadBranchIcon } = require("../../utils/multer");
 
 // # create, update, view, list, activate/inactive, delete shifts  
 
-router.post('/create/shift', entityAuth.authorizeEntity("Administration", "Employee", "create"), shiftController.create);  
+router.post('/create/shift', entityAuth.authorizeEntity("Administration", "Shift", "create"), shiftController.create);  
 
-router.get('/list/shift', entityAuth.authorizeEntity("Administration", "Employee", "create"), shiftController.list);  
+router.get('/list/shift', entityAuth.authorizeEntity("Administration", "Shift", "create"), shiftController.list);  
 
-router.put('/update/shift', entityAuth.authorizeEntity("Administration", "Employee", "update"), shiftController.update);  
+router.put('/update/shift', entityAuth.authorizeEntity("Administration", "Shift", "update"), shiftController.update);  
 
-router.post("/activeInactive/shift", entityAuth.authorizeEntity("Administration", "Employee", "create"), shiftController.activeinactive);  
+router.post("/activeInactive/shift", entityAuth.authorizeEntity("Administration", "Shift", "create"), shiftController.activeinactive);  
 
-// router.get('/shift/:clientId/:shiftId', entityAuth.authorizeEntity("Administration", "Employee", "create"), shiftController.getParticular);  
+// router.get('/shift/:clientId/:shiftId', entityAuth.authorizeEntity("Administration", "Shift", "create"), shiftController.getParticular);  
 
 // # create, update, view, list, activate/inactive shifts  
 
