bbe043d95d38474cc4bc907522911dfec704ff20
Md Shahbaz Ahmad <mdshahbazahmad@Mds-Mac-mini.local>
2025-12-29 17:59:07 +0530

    purhchase payment configure schema design

---
+++ client/model/purchasePaymentConfig.js
@@ -0,0 +1,39 @@
+
+
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+const ObjectId = Schema.ObjectId;
+
+const purhcasePaymentConfigSchema = new Schema(
+    {
+        businessUnit: { type: ObjectId, ref: "businessUnit", default: null, index: true },
+        branch: { type: ObjectId, ref: "branch", default: null, index: true },
+        warehouse: { type: ObjectId, ref: "warehouse", default: null, index: true },
+
+        isVendorLevel: { type: Boolean, default: false },
+        isBuLevel: { type: Boolean, default: false },
+        isBranchLevel: { type: Boolean, default: false },
+        isWarehouseLevel: { type: Boolean, default: false },
+        type: { type: String, enum: ['cash', 'bank'], required: true },
+        ledgers: [
+            {
+                id: { type: mongoose.Schema.Types.ObjectId, ref: 'ledger', default: null },
+                isPrimary: {type: Boolean, default: false}
+            }
+        ],
+        createdBy: { type: ObjectId, ref: "clientUsers", index: true },
+        updatedBy: { type: ObjectId, ref: 'clientUsers' },
+        updatedAt: { type: Date, default: null, index: true },
+        deletedAt: { type: Date, default: null, index: true },
+    },
+    { timestamps: true }
+);
+
+
+module.exports = purhcasePaymentConfigSchema;
+
+
+
+
+
