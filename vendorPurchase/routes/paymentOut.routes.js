77a5669d43bca1da034767089ae2242004a7c1e9
Md Shahbaz Ahmad <mdshahbazahmad@Mds-Mac-mini.local>
2026-01-15 17:57:22 +0530

    middleware changes

--- vendorPurchase/routes/paymentOut.routes.js
+++ vendorPurchase/routes/paymentOut.routes.js
@@ -7,11 +7,11 @@ const entityAuth = require("../../middleware/authorization/commonEntityAuthoriza
 const { uploadBranchIcon } = require("../../utils/multer");
 
 
-router.post('/create/payment/out', entityAuth.authorizeEntity("Administration", "Employee", "create"), paymentOutController.create);
+router.post('/create/payment/out', entityAuth.authorizeEntity("Purchases", "Payment Out", "create"), paymentOutController.create);
 
-router.get('/list/payment/out', entityAuth.authorizeEntity("Administration", "Employee", "create"), paymentOutController.list);
+router.get('/list/payment/out', entityAuth.authorizeEntity("Purchases", "Payment Out", "view"), paymentOutController.list);
 
-router.get('/get/payment/out/:id/:clientId', entityAuth.authorizeEntity("Administration", "Employee", "create"), paymentOutController.getParticularPaymentOut);
+router.get('/get/payment/out/:id/:clientId', entityAuth.authorizeEntity("Purchases", "Payment Out", "view"), paymentOutController.getParticularPaymentOut);
 
 
 exports.router = router; 
--- vendorPurchase/routes/purchaseInvoice.routes.js
+++ vendorPurchase/routes/purchaseInvoice.routes.js
@@ -9,26 +9,26 @@ const { uploadBranchIcon } = require("../../utils/multer");
 
 // # create, update, view, list, activate/inactive purchaseInvoice 
 
-router.post('/create/purchaseInvoice', entityAuth.authorizeEntity("Administration", "Employee", "create"), purchaseInvoiceController.create);
+router.post('/create/purchaseInvoice', entityAuth.authorizeEntity("Purchases", "Purchase Invoices", "create"), purchaseInvoiceController.create);
 
-router.get('/get/audit/purchaseInvoice/:clientId/:purchaseOrderId', entityAuth.authorizeEntity("Administration", "Employee", "create"), purchaseInvoiceController.getAuditPurchaseInvoice);
+router.get('/get/audit/purchaseInvoice/:clientId/:purchaseOrderId', entityAuth.authorizeEntity("Purchases", "Purchase Invoices", "view"), purchaseInvoiceController.getAuditPurchaseInvoice);
 
-router.post('/audit/item/purchaseInvoice', entityAuth.authorizeEntity("Administration", "Employee", "create"), purchaseInvoiceController.auditItem);
-
-
+router.post('/audit/item/purchaseInvoice', entityAuth.authorizeEntity("Purchases", "Purchase Invoices", "create"), purchaseInvoiceController.auditItem);
 
 
-router.post('/issue/purchaseInvoice/mail', entityAuth.authorizeEntity("Administration", "Employee", "create"), purchaseInvoiceController.issueMail);
 
-router.put('/update/purchaseInvoice', entityAuth.authorizeEntity("Administration", "Employee", "update"), purchaseInvoiceController.update);
 
-router.get('/get/:clientId/:purchaseOrderId', entityAuth.authorizeEntity("Administration", "Employee", "create"), purchaseInvoiceController.getParticular);
+router.post('/issue/purchaseInvoice/mail', entityAuth.authorizeEntity("Purchases", "Purchase Invoices", "create"), purchaseInvoiceController.issueMail);
 
-router.get('/list/purchaseInvoice', entityAuth.authorizeEntity("Administration", "Employee", "create"), purchaseInvoiceController.list);
+router.put('/update/purchaseInvoice', entityAuth.authorizeEntity("Purchases", "Purchase Invoices", "create"), purchaseInvoiceController.update);
 
-router.get('/unpaid/purchaseInvoice', entityAuth.authorizeEntity("Administration", "Employee", "create"), purchaseInvoiceController.unpaidInvoices);
+router.get('/get/:clientId/:purchaseOrderId', entityAuth.authorizeEntity("Purchases", "Purchase Invoices", "create"), purchaseInvoiceController.getParticular);
 
-router.post("/change/status/purchaseInvoice", entityAuth.authorizeEntity("Administration", "Employee", "create"), purchaseInvoiceController.changeStatus);
+router.get('/list/purchaseInvoice', entityAuth.authorizeEntity("Purchases", "Purchase Invoices", "create"), purchaseInvoiceController.list);
+
+router.get('/unpaid/purchaseInvoice', entityAuth.authorizeEntity("Purchases", "Purchase Invoices", "create"), purchaseInvoiceController.unpaidInvoices);
+
+router.post("/change/status/purchaseInvoice", entityAuth.authorizeEntity("Purchases", "Purchase Invoices", "create"), purchaseInvoiceController.changeStatus);
 
 
 exports.router = router; 
